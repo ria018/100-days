a5c2f19d16539e49585b731d521d166eae094493
ria018 <riaramadani1810@gmail.com>
2023-10-20 10:08:29 +0700

    biodata

---
+++ Day2.java
@@ -0,0 +1,11 @@
+package day.pkg2;
+public class Day2 {
+    public static void main(String[] args) {
+        String nama = ("Ria Ramadani");
+        String alamat = ("Mamuju Tengah");
+        System.out.println(nama);
+        System.out.println(alamat);
+        
+    }
+    
+}
