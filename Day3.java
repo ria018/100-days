ad5fd07bad846217aa768b05daa16e2c1ed83620
ria018 <riaramadani1810@gmail.com>
2023-10-20 10:13:48 +0700

    Integer

---
+++ Day3.java
@@ -0,0 +1,12 @@
+package day3;
+public class Day3 {
+    public static void main(String[] args) {
+        int angka = 18;
+        int angka2 = 10;
+        int angka3 = 4;
+        System.out.println(angka + angka2 + angka3);
+        System.out.println(angka + angka2);
+        System.out.println(angka + angka3);
+    }
+    
+}
