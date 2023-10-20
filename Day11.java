a927984af2f08aa766a52f8206f3b946ce8f6505
ria018 <riaramadani1810@gmail.com>
2023-10-20 10:26:52 +0700

    type casting

---
+++ Day11.java
@@ -0,0 +1,26 @@
+package day11;
+public class Day11 {
+    public static void main(String[] args) {
+       //short ke double
+        short variable1 = 154;
+        double double_variable = variable1; 
+        System.out.println("Short ke Double: " + double_variable);
+        //char ke long
+        char variable2 = 'A';
+        long long_variable = variable2;
+        System.out.println("Char ke Long: " + long_variable);
+        // int ke float
+        int  variable3 = 204;
+        float int_variable = variable3;
+        System.out.println("Int ke Float: " + int_variable);
+        //byte ke long
+        byte variable4 = 123;
+        long long_variable2 = variable4;
+        System.out.println("Byte ke Long: " + long_variable2);
+        //float ke double
+        float variable5 = 333.3f;
+        double double_variable2 = variable5;
+        System.out.println("Float ke Double: " + double_variable2); 
+    }
+}
+ 
