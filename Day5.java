b8cc6f5815af827d296e4605d5542aba2c134e06
ria018 <riaramadani1810@gmail.com>
2023-10-20 10:16:53 +0700

    Aritmatika

---
+++ Day5.java
@@ -0,0 +1,22 @@
+package aritmatika;
+
+public class Aritmatika {
+
+    public static void main (String [] args) {
+        int penjumlahan ,pengurangan ,perkalian ,pembagian ,hasilBagi;
+        
+        penjumlahan = 10 + 8;
+        pengurangan = 2023 - 2004;
+        perkalian   = 5 * 9;
+        pembagian   = 72_000 / 3600;
+        hasilBagi   = 15 % 4;
+        
+        System.out.println("Hasil dari penjumlahan 10 + 8       = " + penjumlahan);
+        System.out.println("Hasil dari pengurangan 2023 - 2004  = " + pengurangan);
+        System.out.println("Hasil dari Perkalian 5 x 9          = " + perkalian);
+        System.out.println("Hasil dari pembagian 72,000 / 3600  = " + pembagian);
+        System.out.println("Hasil Bagi dari 15 & 4              = " + hasilBagi);
+                
+    }
+    
+}
