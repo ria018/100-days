af96e60004504591f66a56b19d11f8e0d39e4fe0
ria018 <riaramadani1810@gmail.com>
2023-10-20 10:15:44 +0700

    Biodata

---
+++ Day4.java
@@ -0,0 +1,21 @@
+package biodata;
+public class Biodata {
+    public static void main(String[] args) {
+     String nama = "Ria Ramadani";
+        String alamat = "Mamuju Tengah";
+        byte umur = 18;
+        double tinggiBadan = 155.0;
+        float beratBadan = 40.0f;
+        boolean mahasiswa = true;
+        String angkatan = "2023";
+        
+        System.out.println("Nama          : " + nama);
+        System.out.println("Alamat        : " + alamat);
+        System.out.println("Umur          : " + umur);
+        System.out.println("Tinggi Badan  : " + tinggiBadan + "cm");
+        System.out.println("Berat Badan   : " + beratBadan + "kg" );
+        System.out.println("Mahasiswa     : " + mahasiswa);
+        System.out.println("Angkatan Tahun: " + angkatan);   
+    }
+    
+}
