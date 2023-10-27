64e5c19ce8ed6f9d3d439a6afb6387e123375aff
ria018 <riaramadani1810@gmail.com>
2023-10-27 19:04:28 +0700

    Perulangan for

--- Day19.java
+++ Day19.java
@@ -1,8 +1,8 @@
 package day19;
-import java.util.Scanner;
+   import java.util.Scanner;
 public class Day19 {
     public static void main(String[] args) {
-        for (int i =2; i <= 10; i++) {
+      for (int i = 2; i <= 15; i++) {
     System.out.println(i);
 }
     }
