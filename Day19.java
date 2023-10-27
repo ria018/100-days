78c99f9a5b43c2d58be123be685fa12cee0dd198
ria018 <riaramadani1810@gmail.com>
2023-10-27 18:40:16 +0700

    Rule switch singkat

---
+++ Day19.java
@@ -0,0 +1,10 @@
+package day19;
+import java.util.Scanner;
+public class Day19 {
+    public static void main(String[] args) {
+        for (int i =2; i <= 10; i++) {
+    System.out.println(i);
+}
+    }
+    
+}
