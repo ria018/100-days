3ce30ceca34231f772a2e8a26d6dc1a9a4681ec9
ria018 <riaramadani1810@gmail.com>
2023-10-28 17:55:29 +0700

    for down to

---
+++ Day20.java
@@ -0,0 +1,9 @@
+package day20;
+public class Day20 {
+    public static void main(String[] args) {
+        for (int i = 20; i >= 15; i--) {
+            System.out.println(i);
+        }
+    }
+    
+}
