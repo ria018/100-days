e845cde1212bec94f822057c5d41c3f6cf5f3a21
ria018 <riaramadani1810@gmail.com>
2023-10-28 21:25:26 +0700

    pajak

--- Day20.java
+++ Day20.java
@@ -1,8 +1,23 @@
 package day20;
+
+import java.util.Scanner;
+
 public class Day20 {
     public static void main(String[] args) {
-        for (int i = 20; i >= 15; i--) {
-            System.out.println(i);
+        Scanner input = new Scanner (System.in);
+        System.out.println("masukkan gaji");
+        double gaji = input.nextDouble();
+        System.out.println("gaji" + gaji);
+        
+        if (gaji >= 3000000 && gaji < 5000000){
+            System.out.println("pajak 5%");
+            System.out.println(gaji - (gaji*0.5));
+        }else if (gaji > 5000000){
+            System.out.println("pajak 10%");
+            System.out.println(gaji - (gaji*0.1));
+        }else{
+            System.out.println("tidak kena pajak");
+            
         }
     }
     
