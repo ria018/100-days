167a0e88610778a3a506024467b3f09b765083d9
ria018 <riaramadani1810@gmail.com>
2023-10-30 19:57:46 +0700

    for increment

---
+++ Day22.java
@@ -0,0 +1,11 @@
+package day22;
+public class Day22 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 30; i+=4) {
+            System.out.println(i);
+       
+           
+        }
+    }
+    
+}
