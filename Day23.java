b699f58a8b0fbf4d818b42ac49461aefa956758f
ria018 <riaramadani1810@gmail.com>
2023-10-31 21:19:36 +0700

    for decrement

---
+++ Day23.java
@@ -0,0 +1,10 @@
+package day23;
+public class Day23 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 5; i-=5) {
+            System.out.println(i);
+        }
+      
+    }
+    
+}
