08ea27ebae40ea9995f022c83f9207937028e3db
ria018 <riaramadani1810@gmail.com>
2023-11-01 22:41:29 +0700

    While

---
+++ Day24.java
@@ -0,0 +1,11 @@
+package day24;
+public class Day24 {
+    public static void main(String[] args) {
+        int angkaGenap = 4;
+        while (angkaGenap <=  56){
+            System.out.println(angkaGenap);
+            angkaGenap+=6;
+        }
+    }
+    
+}
