08521a20de724dbb09ff0ce3f591380602665105
ria018 <riaramadani1810@gmail.com>
2023-11-02 18:39:04 +0700

    Do while

---
+++ Day25.java
@@ -0,0 +1,11 @@
+package day25;
+public class Day25 {
+    public static void main(String[] args) {
+        int ganjil = 3;
+
+        do{System.out.println("Angka" + ganjil);
+           ganjil+=2;
+     }while( ganjil <= 10);
+    }
+    
+}
