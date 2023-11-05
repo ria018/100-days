b4b208f4575c2048c6ec88743abe3a0d476a544b
ria018 <riaramadani1810@gmail.com>
2023-11-05 16:31:44 +0700

    Memeriksa angka positif, negatif, atau nol

--- Day28.java
+++ Day28.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Day28 {
     public static void main(String[] args) {
-       Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner (System.in);
        int angka = sc.nextInt();
        
        if (angka>0){
