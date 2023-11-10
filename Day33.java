6ada44b41adfabeb6f02ab4bda1751f6af3e0e63
ria018 <riaramadani1810@gmail.com>
2023-11-10 21:03:35 +0700

    perulangan for yang habis dibagi 4

---
+++ Day33.java
@@ -0,0 +1,12 @@
+package day33;
+public class Day33 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            if (i % 2 == 0 && i % 4 == 0) {
+                System.out.println(i);
+            }
+        }
+                
+            }
+            
+        }
