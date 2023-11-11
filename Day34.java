1f2237902e18243d90bf8f66448210396aafd9a4
ria018 <riaramadani1810@gmail.com>
2023-11-11 21:12:33 +0700

    Membuat array 10 angka genap dan menjumlahkannya

---
+++ Day34.java
@@ -0,0 +1,22 @@
+package day34;
+public class Day34 {
+    public static void main(String[] args) {
+    int[] arrayAngkaGenap = new int [10];
+        for (int i = 0, angka = 2; i < 10; angka += 2,i++) {
+            arrayAngkaGenap [i] = angka;
+            
+        }
+            int jumlah = 0;
+            for (int angka : arrayAngkaGenap){
+                jumlah += angka;
+                
+        }
+        System.out.println("Array Angka Genap");
+        for (int angka : arrayAngkaGenap){
+            System.out.print(angka + " ");
+        }
+        System.out.println("\nJumlah angka genap" + jumlah);
+    }
+   
+    
+}
