af0b27e6928898a433fb5e08e5f2e550b1678d6a
ria018 <riaramadani1810@gmail.com>
2023-11-13 20:12:01 +0700

    Membuat array 11 angka negatif dan menjumlahkannya

--- Day36.java
+++ Day36.java
@@ -1,10 +1,10 @@
-package Coding;
+package day36;
 public class Day36 {
     public static void main(String[] args) {
-        int[] arrayAngkaNegatif = new int [10];
+      int[] arrayAngkaNegatif = new int [11];
               
         int angka = -1;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 11; i++) {
             arrayAngkaNegatif[i] = angka;
             angka --;
         }
