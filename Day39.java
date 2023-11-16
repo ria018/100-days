07a3dc1851583bcd6c5bdfbc84b5c0a5f37eb4ff
ria018 <riaramadani1810@gmail.com>
2023-11-16 21:48:03 +0700

    Memeriksa ganjil genap perulangan for

--- Day39.java
+++ Day39.java
@@ -1,7 +1,7 @@
 package day39;
 public class Day39 {
     public static void main(String[] args) {
-       for (int i = 1; i <= 15; i++) {
+         for (int i = 1; i <= 10; i++) {
             if (i%2 == 0){
                 System.out.println(i + "genap");
             }else{
@@ -9,9 +9,6 @@ public class Day39 {
             }
             
         }
-       
-    }
-        
     }
     
-
+}
