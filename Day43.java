bbad3c9be00f8fbbfac578053ff102b4eb478bc8
ria018 <riaramadani1810@gmail.com>
2023-11-20 22:11:11 +0700

    Switch case (Musim)

---
+++ Day43.java
@@ -0,0 +1,45 @@
+package day43;
+
+import java.util.Scanner;
+
+public class Day43 {
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan nomor bulan (1-12): ");
+        int nomorBulan = sc.nextInt();
+
+        String musim;
+
+        switch (nomorBulan) {
+            case 12:
+            case 1:
+            case 2:
+                musim = "Musim Dingin";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                musim = "Musim Semi";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                musim = "Musim Panas";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                musim = "Musim Gugur";
+                break;
+            default:
+                musim = "Bulan tidak valid";
+                break;
+        }
+
+        System.out.println("Bulan ke-" + nomorBulan + " terjadi pada musim " + musim);
+
+        sc.close();
+    }
+    
+}
