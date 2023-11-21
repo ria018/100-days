7ee784b53d17ec38ea0b8725010279ecb255783a
ria018 <riaramadani1810@gmail.com>
2023-11-21 23:14:39 +0700

    perulangan (break)

---
+++ Day44.java
@@ -0,0 +1,14 @@
+package day44;
+public class Day44 {
+    public static void main(String[] args) {
+        for (int R = 0; R < 9; R++) {
+            if (R >= 6){
+                break;
+            }else{
+                System.out.print(R);
+            }
+            
+        }
+    }
+    
+}
