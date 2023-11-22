fcab2cb7e9a0b57591029902348679c9e0cde8b7
ria018 <riaramadani1810@gmail.com>
2023-11-22 15:39:06 +0700

    Perulangan (continue)

---
+++ Day45.java
@@ -0,0 +1,14 @@
+package day45;
+
+public class Day45 {
+
+    public static void main(String[] args) {
+        for (int A = 0; A < 8; A++) {
+            if (A == 5) {
+                continue;
+            } else {
+                System.out.print(A);
+            }
+        }
+    }
+}
