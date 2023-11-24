8a150e6ae0639758d712d540053950cc668dccff
ria018 <riaramadani1810@gmail.com>
2023-11-24 20:49:59 +0700

    Do while

---
+++ Day47.java
@@ -0,0 +1,13 @@
+package day47;
+
+public class Day47 {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.print(i);
+            i++;
+        } while (i <= 5);
+    }
+}
