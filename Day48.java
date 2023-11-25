02c26e6309e86c52fc788b8231103cbb26f65750
ria018 <riaramadani1810@gmail.com>
2023-11-25 21:27:05 +0700

    pola bintang (segi empat)

---
+++ Day48.java
@@ -0,0 +1,13 @@
+package day48;
+public class Day48 {
+    public static void main(String[] args) {
+       int tinggi = 4; 
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= tinggi; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
