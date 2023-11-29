78634730934fb9ff10a9f2e15f967b538949006a
ria018 <riaramadani1810@gmail.com>
2023-11-29 20:13:29 +0700

    Array (perulangan for)

---
+++ Day52.java
@@ -0,0 +1,13 @@
+package day52;
+
+public class Day52 {
+
+    public static void main(String[] args) {
+        String[] nama = {"Nurul Imaniar", "Ahmad Suardi", "Febri Eka Wahyuni", "Nur Avika", "Ria Ramadani"};
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println(nama[i]);
+
+        }
+    }
+
+}
