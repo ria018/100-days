5df16c9aafa53fc498695069f6cd94ee2884c0b3
ria018 <riaramadani1810@gmail.com>
2023-12-05 21:59:54 +0700

    mengisi sebuah array 2D berukuran 3x3

---
+++ Day58.java
@@ -0,0 +1,23 @@
+package day58;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day58 {
+
+    public static void main(String[] args) {
+        int[][] data = new int [3][3];
+        Scanner sc = new Scanner (System.in);
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.println("masukkan angka ["+i+"]["+j+"]");
+                data[i][j] = sc.nextInt();
+                
+            }
+        }
+        System.out.println("--------------");
+        System.out.println(Arrays.deepToString(data));
+            }
+            
+        }
+    
