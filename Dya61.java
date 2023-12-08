07f5ca16d7db19185033e473381f54545e6bda18
ria018 <riaramadani1810@gmail.com>
2023-12-08 22:09:02 +0700

    Rundown acara

---
+++ Dya61.java
@@ -0,0 +1,33 @@
+package dya61;
+
+import java.util.Scanner;
+
+public class Dya61 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan judul acara : ");
+        String judulAcara = input.nextLine();
+
+        System.out.println("\nRundown Acara : " + judulAcara);
+        System.out.println("----------------------------------");
+
+        while (true) {
+            System.out.println("Masukkan waktu acara (hh:mm) atau ketik 'selesai' untuk mengakhiri : ");
+            String waktuAcara = input.nextLine();
+
+            if (waktuAcara.equalsIgnoreCase("selesai")) {
+                break;
+            }
+            System.out.println("Masukkan deskripsi acara : ");
+            String deskripsiAcara = input.nextLine();
+
+            System.out.println("\n" + waktuAcara + " - " + deskripsiAcara);
+
+        }
+        System.out.println("\nRundown acara " + judulAcara + "selesai.");
+
+    }
+
+}
