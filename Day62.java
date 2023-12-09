4d18d6b3875e45acec0db87a4b385649beecfcfc
ria018 <riaramadani1810@gmail.com>
2023-12-09 20:48:57 +0700

    Belanja

---
+++ Day62.java
@@ -0,0 +1,70 @@
+package day62;
+
+import java.util.Scanner;
+
+public class Day62 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Selamat datang di Alfamart!");
+        System.out.println("Masukkan nama Anda : ");
+        String namaPelanggan = input.nextLine();
+
+        System.out.println("\nDftar barang : ");
+        System.out.println("1. Rinso - Rp. 10,000");
+        System.out.println("2. Sabun mandi - Rp. 5,000");
+        System.out.println("3. Mie instan - Rp. 10,000");
+        System.out.println("4. Beras 10kg - Rp. 130,000");
+
+        System.out.println("\nSilahkan masukkan pilihan  belanja Anda (1-4), atau 0 untuk selesai : ");
+
+        int totalHarga = 0;
+
+        while (true) {
+            System.out.println("Pilihan barang (0 untuk selesai) : ");
+            int pilihan = input.nextInt();
+
+            if (pilihan == 0) {
+                break;
+            }
+            int hargaBarang = 0;
+            String namaBarang = " ";
+
+            switch (pilihan) {
+                case 1:
+                    namaBarang = "Rinso";
+                    hargaBarang = 10000;
+                    break;
+                case 2:
+                    namaBarang = "Sabun mandi";
+                    hargaBarang = 5000;
+                    break;
+                case 3:
+                    namaBarang = "Mie instan";
+                    hargaBarang = 10000;
+                    break;
+                case 4:
+                    namaBarang = "Beras 10kg";
+                    hargaBarang = 130000;
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silahkan pilih yang lain lagi.");
+                    continue;
+            }
+
+            System.out.print("Jumlah barang yang dibeli : ");
+            int jumlahBarang = input.nextInt();
+
+            int subtotal = hargaBarang * jumlahBarang;
+            totalHarga += subtotal;
+
+            System.out.println(namaBarang + " x " + jumlahBarang + " = Rp. " + subtotal);
+
+        }
+        System.out.println("\nTerima kasih, " + namaPelanggan + "!");
+        System.out.println("Total belanja Anda : Rp. " + totalHarga);
+    }
+
+}
+
