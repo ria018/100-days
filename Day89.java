577a61f31263b43bec5f8eb5e4856b032bd84731
ria018 <riaramadani1810@gmail.com>
2024-01-05 20:35:58 +0700

    Day_89

---
+++ Day89.java
@@ -0,0 +1,11 @@
+package day89;
+public class Day89 {
+    public static void main(String[] args) {
+        String[] buah = {"mangga", "durian", "jeruk", "kelapa"};
+        for (int i = 0; i < buah.length; i++) {
+            System.out.println("Buah" + i + ": " + buah[i]);
+            
+        }
+    }
+    
+}
