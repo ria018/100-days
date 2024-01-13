39912d92fa3d58cfe989a4bdbe3aa4283ffeddcc
ria018 <riaramadani1810@gmail.com>
2024-01-13 08:32:16 +0700

    Day_97

---
+++ Day97.java
@@ -0,0 +1,17 @@
+package day97;
+
+public class Day97 {
+
+    public static void main(String[] args, int angka) {
+        for (int i = angka; i >= 1; i -= 2) {
+            System.out.println(i);
+
+        }
+
+    }
+
+    public static void perulangan(int angka) {
+        perulangan(35);
+    }
+
+}
