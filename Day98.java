20501463ab02263e41f033ecb69994b11fb9f997
ria018 <riaramadani1810@gmail.com>
2024-01-14 21:00:48 +0700

    Day_98

---
+++ Day98.java
@@ -0,0 +1,16 @@
+package day98;
+
+public class Day98 {
+
+    public static void main(String[] args) {
+        String kata = "Ria Ramadani";
+
+        char karakter1 = kata.charAt(0);
+        char karakter2 = kata.charAt(5);
+
+        System.out.println("Karakter pada index 0 : " + karakter1);
+        System.out.println("Karakter pada index 7 : " + karakter2);
+
+    }
+
+}
